3d94cacb5060fe6da714118f2fd6e9692c6c0d1e
Kassia1971 <katarzyna.gil20@onet.pl>
2020-06-03 23:16:59 +0200

    Todo style

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -86,3 +86,68 @@
         }
     };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Share+Tech+Mono');
+
+$color: rgb(0, 150, 200);
+$color-light: white;
+#clock {
+  order: 0;
+  flex: 0 1 auto;
+  align-self: center;
+
+  color: $color;
+  //text-shadow: 0px 0px 25px $color;
+
+  .time {
+    font-size: 6.5em;
+  }
+
+  .text {
+    margin-top: 30px;
+    font-size: 1em;
+    color: rgba($color, .15);
+    text-align: center;
+
+    a {
+      text-decoration: none;
+      color: inherit;
+
+      transition: color .1s ease-out;
+
+      &:hover {
+        color: $color;
+      }
+    }
+  }
+
+  .btn-container {
+    display: flex;
+    margin-top: 15px;
+
+    a {
+      text-align: center;
+      font-family: 'Share Tech Mono', sans-serif;
+      background: transparent;
+      //border: 3px solid $color;
+      border: none;
+      color: $color;
+      padding: 10px 15px;
+      margin: 0 10px;
+      text-transform: uppercase;
+      font-size: 2em;
+      cursor: pointer;
+      //text-shadow: 0px 0px 10px $color;
+
+      flex-grow: 1;
+
+      transition: color .1s ease-out;
+
+      &:hover {
+        color: $color-light;
+      }
+    }
+  }
+}
+</style>
